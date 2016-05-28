9fdfca1db6e1babf6f6d08cadd0be271c1d1a3c5
Dosdos <dav.santucci@gmail.com>
2016-05-29 01:11:56 +0200

    Implement urls and views for REAT api

--- bot-server/bot/views.py
+++ bot-server/bot/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from rest_framework import viewsets
+from .models import Team, Player, Match
+from .serializers import TeamSerializer, PlayerSerializer, MatchSerializer
 
 
 # ===========================================================================
@@ -9,3 +12,22 @@ def home(request, template_name="bot/home.html"):
     return render_to_response(template_name, {
         'session': request.session.keys(),
     }, context_instance=RequestContext(request))
+
+
+#########################################
+# Django REST framework                 #
+# http://www.django-rest-framework.org/ #
+#########################################
+class TeamViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+
+class PlayerViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+
+class MatchViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Match.objects.all()
+    serializer_class = MatchSerializer
